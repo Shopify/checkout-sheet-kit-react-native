c3e24a434f2aa3bf383cf7fcc705af957176617f
Mark Murray <mark.murray@shopify.com>
2023-11-14 16:38:39 +0000

    Fix swift lint issues

--- ios/ReactNativeShopifyCheckout.swift
+++ ios/ReactNativeShopifyCheckout.swift
@@ -1,25 +1,25 @@
 /*
-MIT License
+ MIT License
 
-Copyright 2023 - Present, Shopify Inc.
+ Copyright 2023 - Present, Shopify Inc.
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 
 import Foundation
 import ShopifyCheckout
@@ -29,7 +29,7 @@ import UIKit
 class RCTShopifyCheckout: UIViewController, CheckoutDelegate {
     func checkoutDidComplete() {}
 
-    func checkoutDidFail(error: ShopifyCheckout.CheckoutError) {}
+    func checkoutDidFail(error _: ShopifyCheckout.CheckoutError) {}
 
     func checkoutDidCancel() {
         DispatchQueue.main.async {
@@ -39,14 +39,14 @@ class RCTShopifyCheckout: UIViewController, CheckoutDelegate {
         }
     }
 
-    // TODO @markmur - Improve return type here
+    // TODO: @markmur - Improve return type here
     @objc func constantsToExport() -> [AnyHashable: Any]! {
         return [
             "version": ShopifyCheckout.version,
         ]
     }
 
-    @objc func present(_ checkoutURL: String) -> Void {
+    @objc func present(_ checkoutURL: String) {
         DispatchQueue.main.async {
             let sharedDelegate = UIApplication.shared.delegate
 
@@ -56,7 +56,7 @@ class RCTShopifyCheckout: UIViewController, CheckoutDelegate {
         }
     }
 
-    @objc func preload(_ checkoutURL: String) -> Void {
+    @objc func preload(_ checkoutURL: String) {
         DispatchQueue.main.async {
             if let url = URL(string: checkoutURL) {
                 ShopifyCheckout.preload(checkout: url)
@@ -79,19 +79,19 @@ class RCTShopifyCheckout: UIViewController, CheckoutDelegate {
         }
     }
 
-    // TODO @markmur - Improve argument type here
-    @objc func configure(_ configuration: [AnyHashable: Any]) -> Void {
-         if let preloading = configuration["preloading"] as? Bool {
-             ShopifyCheckout.configuration.preloading.enabled = preloading
-         }
+    // TODO: @markmur - Improve argument type here
+    @objc func configure(_ configuration: [AnyHashable: Any]) {
+        if let preloading = configuration["preloading"] as? Bool {
+            ShopifyCheckout.configuration.preloading.enabled = preloading
+        }
 
         if let colorScheme = configuration["colorScheme"] as? String {
             ShopifyCheckout.configuration.colorScheme = getColorScheme(colorScheme)
         }
     }
 
-    @objc func getConfig(_ resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) {
-        // TODO @markmur - Improve type here
+    @objc func getConfig(_ resolve: @escaping RCTPromiseResolveBlock, reject _: @escaping RCTPromiseRejectBlock) {
+        // TODO: @markmur - Improve type here
         let config: [AnyHashable: Any]! = [
             "preloading": ShopifyCheckout.configuration.preloading.enabled,
             "colorScheme": ShopifyCheckout.configuration.colorScheme.rawValue,
