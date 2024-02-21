bc2ef88ffcc85ff9b200ef0c363f66bd31af261d
Mark Murray <mark.murray@shopify.com>
2024-02-21 12:34:49 +0000

    Fix issue with frozen checkout state on dismiss

--- modules/@shopify/checkout-sheet-kit/ios/ShopifyCheckoutSheetKit.swift
+++ modules/@shopify/checkout-sheet-kit/ios/ShopifyCheckoutSheetKit.swift
@@ -28,13 +28,7 @@ import React
 
 @objc(RCTShopifyCheckoutSheetKit)
 class RCTShopifyCheckoutSheetKit: RCTEventEmitter, CheckoutDelegate {
-	private var viewControlller: UIViewController?
-
 	private var hasListeners = false
-
-	override init() {
-		super.init()
-	}
 
 	override var methodQueue: DispatchQueue! {
 		return DispatchQueue.main
@@ -89,8 +83,8 @@ class RCTShopifyCheckoutSheetKit: RCTEventEmitter, CheckoutDelegate {
 			if self.hasListeners {
 				self.sendEvent(withName: "close", body: nil)
 			}
-			self.viewControlller?.dismiss(animated: true)
-			self.viewControlller = nil
+
+			self.getCurrentViewController()?.dismiss(animated: true)
 		}
 	}
 
@@ -128,7 +122,6 @@ class RCTShopifyCheckoutSheetKit: RCTEventEmitter, CheckoutDelegate {
 		DispatchQueue.main.async {
 			if let url = URL(string: checkoutURL), let viewController = self.getCurrentViewController() {
 				ShopifyCheckoutSheetKit.present(checkout: url, from: viewController, delegate: self)
-				self.viewControlller = viewController
 			}
 		}
 	}
