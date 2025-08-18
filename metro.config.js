a308e98d78cdada52001a48d5c74f31167eab832
Mark Murray <2034704+markmur@users.noreply.github.com>
2025-08-18 09:34:21 +0100

    Remove redundant blacklist (#267)

--- metro.config.js
+++ metro.config.js
@@ -1,13 +1,9 @@
 const path = require('path');
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
-const escape = require('escape-string-regexp');
-const exclusionList = require('metro-config/src/defaults/exclusionList');
-const pkg = require('./package.json');
 
 const root = path.resolve(__dirname);
 const sample = path.resolve(root, 'sample');
 
-const modules = Object.keys({...pkg.peerDependencies});
 /**
  * Metro configuration
  *  https://reactnative.dev/docs/metro
@@ -19,15 +15,7 @@ const config = mergeConfig(getDefaultConfig(__dirname), {
 
   watchFolders: [root],
 
-  // We need to make sure that only one version is loaded for peerDependencies
-  // So we block them at the root, and alias them to the versions in example's node_modules
   resolver: {
-    blacklistRE: exclusionList(
-      modules.map(
-        m => new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`),
-      ),
-    ),
-
     extraNodeModules: {
       react: path.resolve(sample, 'node_modules', 'react'),
       'react-native': path.resolve(sample, 'node_modules', 'react-native'),
